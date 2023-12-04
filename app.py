36a714855bbd98064ee15322c0131aa3c4e09319
akshayrama <akshayramakrishnan10@gmail.com>
2023-12-04 11:00:11 -0500

    working model - need to optimize

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import io
-from torchvision import models, transforms
-import torch.nn
-from torch import max, unsqueeze
-import os
+from keras.preprocessing import image
+from keras.applications.xception import preprocess_input, decode_predictions
+from keras.applications.xception import Xception
+import numpy as np
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -18,32 +19,24 @@ def predict():
         img_bytes = image_file.read()
         img = Image.open(io.BytesIO(img_bytes))
 
+        img = img.resize((299,299))
+
         # Loading the model and stuff
 
-        model = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
-        transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )])
+        model = Xception(include_top=True)
+        model.load_weights("models/xception_weights_tf_dim_ordering_tf_kernels.h5")
 
-        img_t = transform(img)
-        batch_t = unsqueeze(img_t, 0)
-        out = model(batch_t)
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        features = model.predict(x)
 
-        with open('imagenet_classes.txt') as f:
-            classes = [line.strip() for line in f.readlines()]
+        label = decode_predictions(features, top=1)
 
-        _, index = max(out, 1)
+        label = label[0][0][1]
+        label = label.replace('_', ' ')
 
-        x = classes[index[0]].split(',')[1].strip().replace('_', ' ').title()
-
-        return jsonify({'result': x})
-
+        return jsonify({'result': label.title()})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5050))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, port = 9000, host = '0.0.0.0')
